fef53d5657da4ae95bbd5ee77cccc134e546d473
AdrienHq <adrienhecq@gmail.com>
2022-04-22 16:26:05 +0200

    feat: base and index basic

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,12 @@
 import '../css/app.scss';
+import {Dropdown} from "bootstrap";
+
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdown();
+})
+const enableDropdown = () => {
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    dropdownElementList.map(function (dropdownToggleEl) {
+        return new Dropdown(dropdownEl)
+    });
+}
