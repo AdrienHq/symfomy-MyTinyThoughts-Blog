8fdafad467711141739704f9421be42f6b9f3aba
AdrienHq <adrienhecq@gmail.com>
2022-04-25 12:50:32 +0200

    feat: Back to blog

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,11 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use PhpParser\Node\Expr\Yield_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -38,11 +40,14 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToRoute('Return to the blog', 'fa fa-undo', 'app_home');
+
         yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
             MenuItem::linkToCrud('All articles', '', Article::class),
             MenuItem::linkToCrud('Add', '', Article::class)->setAction(Crud::PAGE_NEW),
             MenuItem::linkToCrud('Categories', '', Category::class)
         ]);
+
+        yield MenuItem::linkToCrud('Comments', 'fas fa-comment', Comment::class);
     }
 }
