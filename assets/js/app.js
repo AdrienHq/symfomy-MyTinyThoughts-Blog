41320d4435bfd7a6f130ffc32a801a60e91411d6
AdrienHq <adrienhecq@gmail.com>
2022-04-25 17:09:03 +0200

    feat: Js Comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,55 @@ import '../css/app.scss';
 import {Dropdown} from "bootstrap";
 
 document.addEventListener('DOMContentLoaded', () => {
-    enableDropdown();
+    new App();
 })
-const enableDropdown = () => {
-    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-    dropdownElementList.map(function (dropdownToggleEl) {
-        return new Dropdown(dropdownEl)
-    });
+
+class App {
+    constructor() {
+        this.enableDropdown();
+        this.handleCommentForm();
+    }
+
+    enableDropdown = () => {
+        const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+        dropdownElementList.map(function (dropdownToggleEl) {
+            return new Dropdown(dropdownToggleEl)
+        });
+    }
+
+    handleCommentForm() {
+        const commentForm = document.querySelector('form.comment-form')
+
+        if(null == commentForm){
+            return;
+        }
+
+        commentForm.addEventListener('submit', async (e) =>{
+            e.preventDefault();
+
+            const response = await fetch('/ajax/comments', {
+                method: 'post',
+                body: new FormData(e.target)
+            });
+
+            if(!response.ok){
+                return;
+            }
+
+            const json = await response.json();
+
+            if(json.code === 'COMMENT_ADDED_SUCCESSFULLY'){
+                const commentList = document.querySelector('.comment-list');
+                const commentCount = document.querySelector('.comment-count');
+                const commentContent = document.querySelector('#comment_content');
+
+                commentList.insertAdjacentHTML('beforeend', json.message);
+                commentList.lastElementChild.scrollIntoView();
+                commentCount.innerText = json.numberOfComments;
+                commentContent.value = '';
+            }
+        })
+    }
 }
+
+
