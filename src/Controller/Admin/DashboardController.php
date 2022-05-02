f3066cb4b0f3bdf644edc0899e7930cf2e7ec39d
AdrienHq <adrienhecq@gmail.com>
2022-05-02 20:35:15 +0200

    feat: User crud admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Menu;
 use App\Entity\Page;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -43,19 +44,30 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToRoute('Return to the blog', 'fa fa-undo', 'app_home');
 
-        yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('All articles', '', Article::class),
-            MenuItem::linkToCrud('Add', '', Article::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Categories', '', Category::class)
-        ]);
+        if($this->isGranted('ROLE_ADMIN')){
+            yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+                MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class),
+                MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class),
+                MenuItem::linkToCrud('Links', 'fas fa-link', Menu::class),
+                MenuItem::linkToCrud('Categories', 'fab fa-delicious', Menu::class),
+            ]);
+        }
 
-        yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
-            MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class),
-            MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class),
-            MenuItem::linkToCrud('Links', 'fas fa-link', Menu::class),
-            MenuItem::linkToCrud('Categories', 'fab fa-delicious', Menu::class),
-        ]);
+        if($this->isGranted('ROLE_AUTHOR')){
+            yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud('All articles', '', Article::class),
+                MenuItem::linkToCrud('Add', '', Article::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Categories', '', Category::class)
+            ]);
+        }
 
-        yield MenuItem::linkToCrud('Comments', 'fas fa-comment', Comment::class);
+        if($this->isGranted('ROLE_ADMIN')){
+            yield MenuItem::linkToCrud('Comments', 'fas fa-comment', Comment::class);
+        }
+
+        yield MenuItem::subMenu('Users accounts', 'fas fa-user')->setSubItems([
+            MenuItem::linkToCrud('All users', 'fas fa-user-friends', User::class),
+            MenuItem::linkToCrud('Add', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW),
+        ]);
     }
 }
