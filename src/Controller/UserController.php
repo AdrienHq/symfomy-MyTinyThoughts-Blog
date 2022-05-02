c7119d0135a10110f4679eb775ab61ad4b86c744
AdrienHq <adrienhecq@gmail.com>
2022-05-02 20:34:54 +0200

    feat: User Crud Controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -15,20 +15,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user/{username}", name="app_profile")
-     */
-    public function index(?User $user): Response
-    {
-        if(!$user){
-            return $this->redirectToRoute('app_home');
-        }
-
-        return $this->render('user/index.html.twig', [
-            'user' => $user
-        ]);
-    }
-
-    /**
      * @Route("/user/register", name="app_register")
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
@@ -80,5 +66,19 @@ class UserController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/user/{username}", name="app_profile")
+     */
+    public function index(?User $user): Response
+    {
+        if(!$user){
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('user/index.html.twig', [
+            'user' => $user
+        ]);
     }
 }
