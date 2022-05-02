390ebc21aeb12e18524d2a062dd945bdee89360b
AdrienHq <adrienhecq@gmail.com>
2022-05-02 23:54:07 +0200

    feat: Admin media

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,15 +5,14 @@ namespace App\Controller\Admin;
 use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Comment;
+use App\Entity\Media;
 use App\Entity\Menu;
-use App\Entity\Page;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-use PhpParser\Node\Expr\Yield_;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -55,9 +54,14 @@ class DashboardController extends AbstractDashboardController
 
         if($this->isGranted('ROLE_AUTHOR')){
             yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
-                MenuItem::linkToCrud('All articles', '', Article::class),
-                MenuItem::linkToCrud('Add', '', Article::class)->setAction(Crud::PAGE_NEW),
-                MenuItem::linkToCrud('Categories', '', Category::class)
+                MenuItem::linkToCrud('All articles', 'fas fa-newspaper', Article::class),
+                MenuItem::linkToCrud('Add', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Categories', 'fas fa-list', Category::class)
+            ]);
+
+            yield MenuItem::subMenu('Medias', 'fas fa-photo-video')->setSubItems([
+                MenuItem::linkToCrud('Media Library', 'fas fa-photo-video', Media::class),
+                MenuItem::linkToCrud('Add Media', 'fas fa-plus', Media::class)->setAction(Crud::PAGE_NEW),
             ]);
         }
 
