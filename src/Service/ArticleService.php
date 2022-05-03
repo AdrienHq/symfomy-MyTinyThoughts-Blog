e6573781f695e2e101f5b08541b79666cc39f7df
AdrienHq <adrienhecq@gmail.com>
2022-05-03 23:34:18 +0200

    feat: Article Service

---
+++ src/Service/ArticleService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class ArticleService
+{
+    private RequestStack $requestStack;
+    private ArticleRepository $articleRepository;
+    private PaginatorInterface $paginator;
+
+    public function __construct(RequestStack $requestStack, ArticleRepository $articleRepository, PaginatorInterface $paginator)
+    {
+        $this->requestStack = $requestStack;
+        $this->articleRepository = $articleRepository;
+        $this->paginator = $paginator;
+    }
+
+    public function getPaginatedArticles(?Category $category = null)
+    {
+        $request = $this->requestStack->getMainRequest();
+        $page = $request->query->getInt('page', 1);
+        $limit = 2;
+        $articleQuery = $this->articleRepository->findForPagination($category);
+
+        return $this->paginator->paginate($articleQuery, $page, $limit);
+
+    }
+
+}
